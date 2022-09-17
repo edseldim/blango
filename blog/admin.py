a72e0a9b57f71ee3feca81c2e15ef50628cd0bc6
Mauve Anteater <edselmanuel@hotmail.com>
2022-09-17 22:42:58 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post, Tag
+from blog.models import Post, Tag, Comment
 # Register your models here.
 admin.site.register(Tag)
 class PostAdmin(admin.ModelAdmin):
@@ -7,3 +7,4 @@ class PostAdmin(admin.ModelAdmin):
   list_display = ["slug","published_at"]
 
 admin.site.register(Post,PostAdmin)
+admin.site.register(Comment)
